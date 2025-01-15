0818a86ae9e0bcfc11c4798e615768f6f357dd48
herb-chan <wujowujo3@gmail.com>
2025-01-16 00:58:18 +0100

    added more perks

--- source/classes/utils/Formulas.py
+++ source/classes/utils/Formulas.py
@@ -3,4 +3,4 @@ class Formulas:
         return (100 * (level + 1)) / wisdom * 10
     
     def gold_per_level_up_formula(self, level):
-        return (100 * (level + 1))  
+        return (100 * (level + 1))
